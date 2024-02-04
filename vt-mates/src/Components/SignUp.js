00049093ea42b270f5e1d817e1cb5e68f838815b
Tecnic69 <74167448+Tecnic69@users.noreply.github.com>
2024-02-04 11:58:23 -0500

    Implementing user add

--- vt-mates/src/Components/SignUp.js
+++ vt-mates/src/Components/SignUp.js
@@ -5,7 +5,7 @@ function SignUp() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const addUser = () => {
+  const addUser = async () => {
     for(let i = 0;i < users.length; i++){
       if(users[i].username === username || users[i].password === password){
         console.log("Username or password already exists")
@@ -17,9 +17,27 @@ function SignUp() {
       username: username,
       password: password
     }
+    try{
+      const response = await fetch('http://localhost:3001/writeFile', {
+        method: 'POST',
+          // sets the type of data stream
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ newUser }),
+      });
 
+      if(response.ok){
+        console.log("User Added");
+      }
+      else{
+        console.log("Failed to add User");
+      }
+    } catch (error){
+      console.log(error);
+    }
     
-  }
+    }
 
     return(
       <div id="sign-up-window">
