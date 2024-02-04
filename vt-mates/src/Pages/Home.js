10080e9d2ce62ec4c4173eba53ea76439dc67b74
Kezie <Leckenby.kenzie@gmail.com>
2024-02-03 23:35:48 -0500

    I broke it, but dw cause now its fixed smiley

--- vt-mates/src/Pages/Home.js
+++ vt-mates/src/Pages/Home.js
@@ -3,7 +3,12 @@ import React from 'react';
 function Home() {
   return(
     <div>
-        Home
+        <h2>Welcome to VTMates!</h2>
+        <p1>VTMates is an app made to help those living in off campus housing rekindle the community that they used to have while staying on campus.</p1>
+        <h2>How it Works</h2>
+        <p1>All you have to do is make a profile and choose some interests and our algorithms will handle the rest. Connecting you with like minded individuals and reccomending potential places to meet up based on your location and interests.</p1>
+        <h2>Sign-up Today</h2>
+        <button>Sign-up</button>
     </div>
   )
 }
