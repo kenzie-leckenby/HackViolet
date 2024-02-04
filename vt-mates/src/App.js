7d02879e76473545302bce5bbea2e8238d71cdfa
Tecnic69 <74167448+Tecnic69@users.noreply.github.com>
2024-02-03 19:53:24 -0500

    converting to react

---
+++ vt-mates/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+
+function App() {
+  return (
+    <div id="nav-bar">
+            <div id="home-button" class="left-side-nav">VTMates</div>
+            <div id="map-button">
+                <FontAwesomeIcon icon={ faEarthAmericas }/>
+            </div>
+            <div class="right-side-nav">
+                <div id="message-button">
+                    <FontAwesomeIcon icon={ faEnvelope } />
+                </div>
+                <div id="profile-button">
+                    <FontAwesomeIcon icon={ faUser } />
+                </div>
+            </div>
+        </div>
+  );
+}
+
+export default App;
